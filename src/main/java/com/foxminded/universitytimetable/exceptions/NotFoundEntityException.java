6bdc07b37374aa2fc41ab7216254147064015004
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2020-04-24 10:13:02 +0300

    Exception for wrong parameter for search

---
+++ src/main/java/com/foxminded/universitytimetable/exceptions/NotFoundEntityException.java
@@ -0,0 +1,30 @@
+package com.foxminded.universitytimetable.exceptions;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+
+public class NotFoundEntityException extends RuntimeException {
+    private EmptyResultDataAccessException emptyResultDataAccessException = null;
+    private String emptyResultExceptionMessage = null;
+
+    public NotFoundEntityException(EmptyResultDataAccessException emptyResultDataAccessException,
+                                   String emptyResultExceptionMessage) {
+        this.emptyResultDataAccessException = emptyResultDataAccessException;
+        this.emptyResultExceptionMessage = emptyResultExceptionMessage;
+    }
+
+    public EmptyResultDataAccessException getEmptyResultDataAccessException() {
+        return emptyResultDataAccessException;
+    }
+
+    public void setEmptyResultDataAccessException(EmptyResultDataAccessException emptyResultDataAccessException) {
+        this.emptyResultDataAccessException = emptyResultDataAccessException;
+    }
+
+    public String getEmptyResultExceptionMessage() {
+        return emptyResultExceptionMessage;
+    }
+
+    public void setEmptyResultExceptionMessage(String emptyResultExceptionMessage) {
+        this.emptyResultExceptionMessage = emptyResultExceptionMessage;
+    }
+}
