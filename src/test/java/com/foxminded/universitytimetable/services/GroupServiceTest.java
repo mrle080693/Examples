1e40df07b11936325d757c9bc067141e352c0f3c
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2020-05-17 16:34:41 +0300

    Done

--- src/test/java/com/foxminded/universitytimetable/services/GroupServiceTest.java
+++ src/test/java/com/foxminded/universitytimetable/services/GroupServiceTest.java
@@ -1,32 +1,84 @@
 package com.foxminded.universitytimetable.services;
 
 import com.foxminded.universitytimetable.configurations.SpringJDBCConfig;
+import com.foxminded.universitytimetable.dao.impl.GroupImpl;
+import com.foxminded.universitytimetable.exceptions.DAOException;
+import com.foxminded.universitytimetable.exceptions.EntityValidationException;
 import com.foxminded.universitytimetable.exceptions.NotFoundEntityException;
 import com.foxminded.universitytimetable.models.Group;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-@Component
 class GroupServiceTest {
-    @Autowired
     private static AnnotationConfigApplicationContext context;
+    private static GroupImpl groupImpl;
     private static GroupService groupService;
+    private Group group;
 
     @BeforeAll
-    static void dataSet() {
+    static void initialize() {
         context = new AnnotationConfigApplicationContext(SpringJDBCConfig.class);
-        groupService = context.getBean("groupServiceBean", GroupService.class);
+    }
+
+    @BeforeEach
+    void dataSet() {
+        group = new Group("Test");
+        groupImpl = Mockito.mock(GroupImpl.class);
+        groupService = new GroupService(groupImpl);
     }
 
     @Test
-    void addMustAddNewGroup() {
-        Group group = new Group("Test");
+    void addMustThrowEntityValidationExceptionIfInputGroupIdIsNotZero() {
+        group.setId(7);
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.add(group));
+    }
+
+    @Test
+    void addMustThrowDAOExceptionIfGroupImplThrowsDataAccessException() {
+        when(groupImpl.add(group)).thenThrow(new DataAccessException("") {
+        });
+        Assertions.assertThrows(DAOException.class, () -> groupService.add(group));
+    }
+
+    @Test
+    void addMustThrowIllegalArgumentExceptionIfInputGroupIsNull() {
+        group = null;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> groupService.add(group));
+    }
+
+    @Test
+    void addMustThrowEntityValidationExceptionIfGroupNameIsNull() {
+        group.setName(null);
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.add(group));
+    }
+
+    @Test
+    void addMustThrowEntityValidationExceptionIfGroupNameIsEmpty() {
+        group.setName("");
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.add(group));
+    }
+
+    @Test
+    void addMustThrowEntityValidationExceptionIfGroupNameIsSeparators() {
+        group.setName("   ");
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.add(group));
+    }
+
+    @Test
+    void addMustReturnTheSameThatGroupImplWasReturn() {
+        when(groupImpl.add(group)).thenReturn(1);
 
         int expected = 1;
         int actual = groupService.add(group);
@@ -34,11 +86,190 @@ class GroupServiceTest {
         assertEquals(expected, actual);
     }
 
-
-
+    @Test
+    void getAllMustThrowsDAOExceptionIfGroupImplThrowsDataAccessException() {
+        when(groupImpl.getAll()).thenThrow(new DataAccessException("") {
+        });
+        Assertions.assertThrows(DAOException.class, () -> groupService.getAll());
+    }
 
     @Test
-    void getByIdMustThrowNotFoundEntityExceptionIfTableIsNotContainsSuchId() {
-        Assertions.assertThrows(NotFoundEntityException.class, () -> groupService.getById(6));
+    void getAllMustThrowNotFoundEntityExceptionIfGroupImplReturnEmptyList() {
+        List<Group> expected = new ArrayList<>();
+
+        when(groupImpl.getAll()).thenReturn(expected);
+
+        Assertions.assertThrows(NotFoundEntityException.class, () -> groupService.getAll());
+    }
+
+    @Test
+    void getAllMustReturnTheSameThatGroupImplWasReturn() {
+        List<Group> expected = new ArrayList<>();
+        expected.add(group);
+
+        when(groupImpl.getAll()).thenReturn(expected);
+
+        List<Group> actual = groupService.getAll();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getByIdMustThrowEntityValidationExceptionIfInputGroupIdIsZero() {
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.getById(0));
+    }
+
+    @Test
+    void getByIdMustThrowNotFoundEntityExceptionIfGroupImplThrowEmptyResultDataAccessException() {
+        when(groupImpl.getById(1)).thenThrow(new EmptyResultDataAccessException(1));
+
+        Assertions.assertThrows(NotFoundEntityException.class, () -> groupService.getById(1));
+    }
+
+    @Test
+    void getByIdMustThrowsDAOExceptionIfGroupImplThrowsDataAccessException() {
+        when(groupImpl.getById(1)).thenThrow(new DataAccessException("") {
+        });
+
+        Assertions.assertThrows(DAOException.class, () -> groupService.getById(1));
+    }
+
+    @Test
+    void getByIdMustReturnTheSameThatGroupImplWasReturn() {
+        Group expected = group;
+
+        when(groupImpl.getById(1)).thenReturn(expected);
+
+        Group actual = groupService.getById(1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getByNameMustThrowEntityValidationExceptionIfInputGroupNameIsNull() {
+        String groupName = null;
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.getByName(groupName));
+    }
+
+    @Test
+    void getByNameMustThrowEntityValidationExceptionIfInputGroupNameIsEmpty() {
+        String groupName = "";
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.getByName(groupName));
+    }
+
+    @Test
+    void getByNameMustThrowEntityValidationExceptionIfInputGroupNameIsSeparators() {
+        String groupName = "     ";
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.getByName(groupName));
+    }
+
+    @Test
+    void getByNameMustThrowNotFoundEntityExceptionIfGroupImplThrowEmptyResultDataAccessException() {
+        when(groupImpl.getByName("Test")).thenThrow(new EmptyResultDataAccessException(1));
+
+        Assertions.assertThrows(NotFoundEntityException.class, () -> groupService.getByName("Test"));
+    }
+
+    @Test
+    void getByNameMustThrowsDAOExceptionIfGroupImplThrowsDataAccessException() {
+        when(groupImpl.getByName("Test")).thenThrow(new DataAccessException("") {
+        });
+
+        Assertions.assertThrows(DAOException.class, () -> groupService.getByName("Test"));
+    }
+
+    @Test
+    void getByNameMustReturnTheSameThatGroupImplWasReturn() {
+        List<Group> expected = new ArrayList<>();
+        expected.add(group);
+
+        when(groupImpl.getByName("Test")).thenReturn(expected);
+
+        List<Group> actual = groupService.getByName("Test");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void updateMustThrowEntityValidationExceptionIfInputGroupIdIsZero() {
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.update(group));
+    }
+
+    @Test
+    void updateMustThrowDAOExceptionIfGroupImplThrowsDataAccessException() {
+        group.setId(1);
+
+        when(groupImpl.update(group)).thenThrow(new DataAccessException("") {
+        });
+
+        Assertions.assertThrows(DAOException.class, () -> groupService.update(group));
+    }
+
+    @Test
+    void updateMustThrowIllegalArgumentExceptionIfInputGroupIsNull() {
+        group = null;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> groupService.update(group));
+    }
+
+    @Test
+    void updateMustThrowEntityValidationExceptionIfGroupNameIsNull() {
+        group.setName(null);
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.update(group));
+    }
+
+    @Test
+    void updateMustThrowEntityValidationExceptionIfGroupNameIsEmpty() {
+        group.setName("");
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.update(group));
+    }
+
+    @Test
+    void updateMustThrowEntityValidationExceptionIfGroupNameIsSeparators() {
+        group.setName("   ");
+        Assertions.assertThrows(EntityValidationException.class, () -> groupService.update(group));
+    }
+
+    @Test
+    void updateMustThrowNotFoundEntityExceptionIfGroupImplReturnStatusNotOne() {
+        group.setId(8);
+
+        when(groupImpl.update(group)).thenReturn(-1);
+
+        Assertions.assertThrows(NotFoundEntityException.class, () -> groupService.update(group));
+    }
+
+    @Test
+    void updateMustReturnTheSameThatGroupImplWasReturn() {
+        group.setId(1);
+
+        when(groupImpl.update(group)).thenReturn(1);
+
+        int expected = 1;
+        int actual = groupService.update(group);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void removeMustThrowNotFoundEntityExceptionIfGroupImplReturnStatusNotOne() {
+        when(groupImpl.remove(7)).thenReturn(-1);
+        Assertions.assertThrows(NotFoundEntityException.class, () -> groupService.remove(1));
+    }
+
+    @Test
+    void removeMustThrowDAOExceptionIfGroupImplThrowsDataAccessException() {
+        when(groupImpl.remove(7)).thenThrow(new DataAccessException("") {
+        });
+        Assertions.assertThrows(DAOException.class, () -> groupService.remove(7));
+    }
+
+    @Test
+    void removeMustReturnStatusOneIfGroupImplWasReturnStatusOne() {
+        int expected = 1;
+        when(groupImpl.remove(1)).thenReturn(expected);
+
+        int actual = groupService.remove(1);
+
+        assertEquals(expected, actual);
     }
 }
