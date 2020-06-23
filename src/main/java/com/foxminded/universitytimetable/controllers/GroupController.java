3005eb7e7c88d68e4c5d477bbdb295fa6048bc3d
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2020-06-23 21:11:15 +0300

    1)Algorithms for add() and getByName() for GroupController were finded. 2) Some changes in groups.js

--- src/main/java/com/foxminded/universitytimetable/controllers/GroupController.java
+++ src/main/java/com/foxminded/universitytimetable/controllers/GroupController.java
@@ -1,55 +1,71 @@
 package com.foxminded.universitytimetable.controllers;
 
-import com.foxminded.universitytimetable.configurations.SpringJDBCConfig;
 import com.foxminded.universitytimetable.models.Group;
 import com.foxminded.universitytimetable.services.GroupService;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
+/*
+I want to see SRD ))
+Not CRUD!!!
+ */
 @Controller
 public class GroupController {
     @Autowired
     GroupService groupService;
+    private Gson gson = new Gson();
 
     @GetMapping("/groups")
     public String getGroupsPage() {
         return "groups";
     }
 
+    @GetMapping("/groups/add")
+    @ResponseBody
+    public String add(JsonObject jsonObject) {
+        Group group = gson.fromJson(jsonObject, Group.class);
+        int newGroupId = groupService.add(group);
+        String result = String.valueOf(newGroupId);
+
+        return result;
+    }
+
     @GetMapping("/groups/getall")
     @ResponseBody
-    public String getAllGroups() {
-        Gson gson = null;
+    public String getAll() {
         List<Group> list = null;
         String result = null;
 
         try {
-            gson = new Gson();
             System.out.println("TRY ))))))))))))))))))))))))))))))))))");
-
             list = groupService.getAll();
             //list = new ArrayList<>();
             System.out.println("DONE ))))))))))))))))))))))))))))))))))");
 
             result = gson.toJson(list);
-        } catch (NullPointerException e) {
+        } catch (Exception e) {
             result = e.getMessage();
         }
 
         if (result.trim().isEmpty()) {
             result = "No groups :(";
         }
+
+        return result;
+    }
+
+    @GetMapping("/groups/getByName")
+    @ResponseBody
+    public String getByName(String inputGroupName) {
+        List<Group> groupsWithInputName = groupService.getByName(inputGroupName);
+        String result = gson.toJson(groupsWithInputName);
 
         return result;
     }
--- src/main/webapp/scripts/groups.js
+++ src/main/webapp/scripts/groups.js
@@ -11,11 +11,13 @@ function getAllGroups() {
 
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("groupMainContent").innerHTML = this.responseText;
 
-    };
-    xhttp.open("GET", "http://localhost:8080/groups/getall", true);
-    xhttp.send();
+        }
+        ;
+        xhttp.open("GET", "http://localhost:8080/groups/getall", true);
+        xhttp.send();
+    }
 }
