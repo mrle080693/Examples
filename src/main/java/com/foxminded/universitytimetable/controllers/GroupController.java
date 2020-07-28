fc2aad272d5379a62c0fd385cc89a27e80cca2f1
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2020-07-28 12:21:19 +0300

    GroupsController works with groups.html

--- src/main/java/com/foxminded/universitytimetable/controllers/GroupController.java
+++ src/main/java/com/foxminded/universitytimetable/controllers/GroupController.java
@@ -2,87 +2,51 @@ package com.foxminded.universitytimetable.controllers;
 
 import com.foxminded.universitytimetable.models.Group;
 import com.foxminded.universitytimetable.services.GroupService;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@Controller("/groups")
 public class GroupController {
     @Autowired
     GroupService groupService;
-    private Gson gson = new Gson();
 
     @GetMapping("/groups")
-    public String getGroupsPage() {
+    public String getGroupsPage(Model model) {
+        List<Group> groups = groupService.getAll();
+
+        model.addAttribute("groups", groups);
+
         return "groups";
     }
 
-    @RequestMapping(value = "/groups/add", method = RequestMethod.POST)
-    @ResponseBody
+    @PostMapping("/groups/add")
     public String add(@RequestParam String groupName) {
         Group group = new Group(groupName);
-        int newGroupId = groupService.add(group);
+        groupService.add(group);
 
-        return String.valueOf(newGroupId);
+        return "redirect:/groups";
     }
 
-    /*
-    ITS WORK BUT WE NEED POST
+    @PostMapping("/groups/update")
+    public String update(@RequestParam int id, @RequestParam String newName) {
+        Group group = new Group(newName);
+        group.setId(id);
 
-    @RequestMapping(value = "/groups/add", method = RequestMethod.GET)
-    public @ResponseBody
-    int add(@RequestBody @RequestParam Group group) {
-        int newGroupId = groupService.add(group);
-        return newGroupId;
-    }
-    */
+        groupService.update(group);
 
-    @GetMapping("/groups/getall")
-    @ResponseBody
-    public String getAll() {
-        List<Group> list = null;
-        String result = null;
-
-        try {
-            System.out.println("TRY ))))))))))))))))))))))))))))))))))");
-            list = groupService.getAll();
-            //list = new ArrayList<>();
-            System.out.println("DONE ))))))))))))))))))))))))))))))))))");
-
-            result = gson.toJson(list);
-        } catch (Exception e) {
-            result = e.getMessage();
-        }
-
-        if (result.trim().isEmpty()) {
-            result = "No groups :(";
-        }
-
-        return result;
+        return "redirect:/groups";
     }
 
-    @RequestMapping(value = "/groups/getByName", method = RequestMethod.GET)
-    @ResponseBody
-    public String getByName(@RequestParam String name) {
-        List<Group> groupsWithInputName = groupService.getByName(name);
-        String result = gson.toJson(groupsWithInputName);
-
-        return result;
+    @PostMapping("/groups/remove")
+    public String remove(@RequestParam int id) {
+        groupService.remove(id);
+        return "redirect:/groups";
     }
 
-    @GetMapping("/groups/getById")
-    @ResponseBody
-    public String getById(@RequestParam String inputId) {
-        int parsedInputId = Integer.valueOf(inputId);
-        Group group = groupService.getById(parsedInputId);
-        String result = gson.toJson(group);
-
-        return result;
-    }
-
-
+    // lessons.html have to return
 }
