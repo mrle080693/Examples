578296f34b5846c5692e1f001ae2bc0e9c2afbbf
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2020-10-29 11:03:12 +0200

    Add Swagger API documentation

--- src/main/java/com/foxminded/universitytimetable/Application.java
+++ src/main/java/com/foxminded/universitytimetable/Application.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 
@@ -25,7 +29,15 @@ public class Application {
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
-
         };
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
