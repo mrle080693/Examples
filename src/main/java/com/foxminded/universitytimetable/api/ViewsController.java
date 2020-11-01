26df605954529c1b4a6eaab478a1ec9c59a9c7b7
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2020-11-01 14:05:03 +0200

    1)Add home url to ViewsController 2)Rewrite api.yaml

--- src/main/java/com/foxminded/universitytimetable/api/ViewsController.java
+++ src/main/java/com/foxminded/universitytimetable/api/ViewsController.java
@@ -7,38 +7,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewsController {
 
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
     @GetMapping(Urls.API_GET_INDEX_HTML)
-    public String getIndex(){
+    public String getIndex() {
         return "index";
     }
 
     @GetMapping(Urls.API_GET_GROUPS_HTML)
-    public String getGroups(){
+    public String getGroups() {
         return "groups";
     }
 
     @GetMapping(Urls.API_GET_LESSONS_HTML)
-    public String getLessons(){
+    public String getLessons() {
         return "lessons";
     }
 
     @GetMapping(Urls.API_GET_PROFESSORS_HTML)
-    public String getProfessors(){
+    public String getProfessors() {
         return "professors";
     }
 
     @GetMapping(Urls.API_GET_STATISTICS_HTML)
-    public String getStatistics(){
+    public String getStatistics() {
         return "statistics";
     }
 
     @GetMapping(Urls.API_GET_TIMETABLE_HTML)
-    public String getTimetable(){
+    public String getTimetable() {
         return "index";
     }
 
     @GetMapping(Urls.API_GET_ERROR_HTML)
-    public String getErrorPage(){
+    public String getErrorPage() {
         return "error-page";
     }
 }
